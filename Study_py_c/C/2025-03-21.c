c5a2ac46e758445cd2e7ae61b9318d291d2ebac0
Hyun731 <gilhyun.gwon0731@gmail.com>
2025-03-21 22:05:15 +0900

    직각삼각형

--- Study_py_c/C/2025-03-21.c
+++ Study_py_c/C/2025-03-21.c
@@ -2,6 +2,10 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
 }
