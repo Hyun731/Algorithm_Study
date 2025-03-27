133dedafc3d390aab156c94e9335c88f30b60855
권길현 <gilhyun.gwon0731@gmail.com>
2025-03-27 23:36:10 +0900

    가독성 향상

--- Data Structure/Queue.py
+++ Data Structure/Queue.py
@@ -2,24 +2,25 @@ class Queue:
     arr : list
     f : int
     r : int
+    size : int
     def __init__(self,size):
         self.arr = [0]*size
         self.f = -1
         self.r = -1
+        self.size = size
     def isFull(self):
-        if self.r == (len(self.arr) - 1): return True
+        if self.r == size - 1: return True
         return False
     def isEmpty(self):
         return self.f == self.r
     def dequeue(self):
         if self.isEmpty():
             return "아무것도 없습니다."
-        else:
-            self.f += 1
-            p = self.arr[self.f]
-            return p
+        self.f += 1
+        p = self.arr[self.f]
+        return p
     def enqueue(self, a):
-        if self.isFull() == False:
+        if not self.isFull():
             self.r += 1
             self.arr[self.r] = a
         else:
