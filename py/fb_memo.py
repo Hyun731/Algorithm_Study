fd41468e69dec9e675807b51544548199154ce20
Hyun731 <gilhyun.gwon0731@gmail.com>
2025-04-21 14:53:28 +0900

    피보나치 메모이제이션

---
+++ py/fb_memo.py
@@ -0,0 +1,15 @@
+memo = {}
+def fibo(n):
+    if n in memo:
+        return memo[n]
+    if n <= 1:
+        return n
+    memo[n] = fibo(n-1) + fibo(n-2)
+    return memo[n]
+
+def roof_fibo(n):
+    if 1 >= n:
+        return n
+    return roof_fibo(n-1) + roof_fibo(n-2)
+
+print(roof_fibo(10))
